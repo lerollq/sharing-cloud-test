a609c37e434343e23dd706e4965c849773ee3a46
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-25 14:47:20 +0200

    add all requests method

--- src/api/methods.ts
+++ src/api/methods.ts
@@ -1,8 +1,30 @@
 import RequestExecutor from './requestExecutor'
-import { LoginResponse, MeResponse } from './types'
+import {
+  GetLoginResponse,
+  GetMeResponse,
+  GetUserByIdResponse,
+  GetResourceResponse,
+  GetBookingsResponse,
+  PostBookingsResponse,
+} from './types'
 
 const requestExecutor = new RequestExecutor()
 
-export const getLogin = () => requestExecutor.get<LoginResponse>('/login')
+export const getLogin = () => requestExecutor.get<GetLoginResponse>('/login')
 
-export const getMe = () => requestExecutor.get<MeResponse>('/me')
+export const getMe = () => requestExecutor.get<GetMeResponse>('/me')
+
+export const getLogout = () => requestExecutor.get<null>('/logout')
+
+export const getReset = () => requestExecutor.get<null>('/logout')
+
+export const geUserById = (id: string) => requestExecutor.get<GetUserByIdResponse>(`/users/${id}`)
+
+export const getResource = () => requestExecutor.get<GetResourceResponse>('/resource')
+
+export const getBookings = () => requestExecutor.get<GetBookingsResponse>('/bookings')
+
+export const postBooking = ({ name, duration }: { name: string; duration: number }) =>
+  requestExecutor.post<PostBookingsResponse>('/bookings', { name, duration })
+
+export const deleteBookings = (bookingId: string) => requestExecutor.delete<null>(`/bookings${bookingId}`)
