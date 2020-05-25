c20583d1d23974f6e706f768d28d3d46afb61789
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-25 14:10:44 +0200

    add generic type in case of success response

--- src/api/requestExecutor.ts
+++ src/api/requestExecutor.ts
@@ -4,40 +4,40 @@ interface BaseResponse {
   success: boolean
 }
 
-interface SuccessResponse extends BaseResponse {
-  data: any
+interface SuccessResponse<T> extends BaseResponse {
+  data: T
 }
 
 interface ErrorResponse extends BaseResponse {
   message: string
 }
 
-type ResponseType = SuccessResponse | ErrorResponse
+type ResponseType<T> = SuccessResponse<T> | ErrorResponse
 
 class RequestExecutor {
   constructor() {}
 
-  public get(path: string): Promise<ResponseType> {
+  public get<T>(path: string): Promise<ResponseType<T>> {
     return this.execRequest(path)
   }
 
-  public post(path: string, body: { [key: string]: any }): Promise<ResponseType> {
-    return this.execRequest(path, { method: 'POST', body: JSON.stringify(body) })
+  public post<T>(path: string, body: { [key: string]: any }): Promise<ResponseType<T>> {
+    return this.execRequest<T>(path, { method: 'POST', body: JSON.stringify(body) })
   }
 
-  private execRequest(
+  private execRequest<T>(
     path: string,
     opts: RequestInit = {
       headers: {
         'Content-Type': 'application/json',
       },
     }
-  ): Promise<ResponseType> {
+  ): Promise<ResponseType<T>> {
     const token = localStorage.getItem('token')
     if (token) {
       Object.assign(opts.headers, { Authorization: `Bearer ${token}` })
     }
-    return fetch(`${environments.api.domain}/${path}`, opts).then((response) => response.json() as Promise<ResponseType>)
+    return fetch(`${environments.api.domain}/${path}`, opts).then((response) => response.json() as Promise<ResponseType<T>>)
   }
 }
 
