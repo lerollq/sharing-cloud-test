54a4e0deaa1915cbbf20bb4204260d7f8bdb10de
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-25 21:15:43 +0200

    update props routing components and default redirection

--- src/components/routing/GuestRoute.tsx
+++ src/components/routing/GuestRoute.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Routes from '../../config/routes'
-import { Route, Redirect, RouteComponentProps } from 'react-router'
+import { PrivateRoutes } from '../../config/routes'
+import { Route, Redirect, RouteComponentProps, RouteProps } from 'react-router'
 
-interface Props {
+interface Props extends RouteProps {
   loggedIn: boolean
   component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>
 }
 
 export const GuestRoute: React.FC<Props> = ({ loggedIn, component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (loggedIn ? <Redirect to={Routes.Dashboard} /> : <Component {...props} />)} />
+  <Route {...rest} render={(props) => (loggedIn ? <Redirect to={PrivateRoutes.Dashboard} /> : <Component {...props} />)} />
 )
--- src/components/routing/PrivateRoute.tsx
+++ src/components/routing/PrivateRoute.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Route, Redirect, RouteComponentProps } from 'react-router'
-import Routes from '../../config/routes'
+import { Route, Redirect, RouteComponentProps, RouteProps } from 'react-router'
+import { GuestRoutes } from '../../config/routes'
 
-interface Props {
+interface Props extends RouteProps {
   loggedIn: boolean
   component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>
 }
 
 export const PrivateRoute: React.FC<Props> = ({ loggedIn, component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (!loggedIn ? <Redirect to={Routes.Home} /> : <Component {...props} />)} />
+  <Route {...rest} render={(props) => (!loggedIn ? <Redirect to={GuestRoutes.Signin} /> : <Component {...props} />)} />
 )
