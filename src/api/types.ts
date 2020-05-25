5fd4cf2c42f7b7ddb480ecd800d97c5592eb4174
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-25 14:46:28 +0200

    add typings for each api response

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,9 +1,32 @@
-export interface LoginResponse {
+export interface GetLoginResponse {
   token: string
   expirationDate: string
 }
 
-export interface MeResponse {
+export interface GetMeResponse {
   id: string
   name: string
+}
+
+export interface GetUserByIdResponse extends GetMeResponse {}
+
+export interface GetResourceResponse {
+  id: string
+  name: string
+  minimumBookingDuration: number
+  maximumBookingDuration: number
+  bookingDurationStep: number
+}
+export interface GetBookingsResponse {
+  [index: number]: {
+    id: string
+    start: string
+    end: string
+    name: string
+    userId: string
+  }
+}
+
+export interface PostBookingsResponse {
+  bookingId: string
 }
