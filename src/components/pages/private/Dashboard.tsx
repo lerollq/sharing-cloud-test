1fa23bae0b9f1f75accdbc2eadb2fb315eb8f9fa
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 21:06:37 +0200

    update dashboard layout

--- src/components/pages/private/Dashboard.tsx
+++ src/components/pages/private/Dashboard.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import CurrentBookings from '../../organisms/CurrentBookings/CurrentBookings'
+import { Row, Col } from '../../atoms/Grid'
+import { ResourceDetails } from '../../organisms/ResourceDetails'
+import { BookForm } from '../../organisms/BookForm'
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>
+  return (
+    <React.Fragment>
+      <Row justifyContent='center'>
+        <Col sm={12} md={4}>
+          <CurrentBookings />
+        </Col>
+        <Col sm={12} md={4}>
+          <Row>
+            <ResourceDetails />
+          </Row>
+          <Row>
+            <BookForm />
+          </Row>
+        </Col>
+      </Row>
+    </React.Fragment>
+  )
 }
 
 export default Dashboard
