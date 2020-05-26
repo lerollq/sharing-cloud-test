cb9ec1fb3892c339fcb6aac985c2c165f6aacc16
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 19:47:45 +0200

    update bookings reducer with new implemented actions

--- src/store/bookings/reducer.ts
+++ src/store/bookings/reducer.ts
@@ -12,9 +12,32 @@ export const reducer: Reducer<InitialState, ActionTypes> = (state = initialState
     case ActionTypeKeys.BOOKINGS_SET:
       return {
         ...state,
-        loaded: true,
-        loading: false,
         bookings: action.payload.bookings,
+      }
+    case ActionTypeKeys.BOOKINGS_ADD:
+      return {
+        ...state,
+        bookings: {
+          ...state.bookings,
+          ...action.payload.booking,
+        },
+      }
+    case ActionTypeKeys.BOOKING_DELETE:
+      console.log(
+        'Booking ID DELETE',
+        action.payload.bookingId,
+        state.bookings.length,
+        state.bookings.filter((b) => b.id !== action.payload.bookingId).length
+      )
+      return {
+        ...state,
+        bookings: state.bookings.filter((b) => b.id !== action.payload.bookingId),
+      }
+    case ActionTypeKeys.BOOKINGS_SET_STATUS:
+      return {
+        ...state,
+        loaded: action.payload.loaded,
+        loading: action.payload.loading,
       }
     default:
       return state
