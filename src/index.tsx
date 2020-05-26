d1e8fbcbb66b066c0cba5b9e01b160270721c9df
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 09:32:00 +0200

    wrap entry point in Router component

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import store from './store'
 import { Provider } from 'react-redux'
+import { GlobalStyle } from './styles/globalStyle'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
+
   document.getElementById('root')
 )
 serviceWorker.unregister()
