dfa4fe9af81c7f58e4d730336aeec8619982e40d
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 21:05:31 +0200

    update global types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,11 @@
 import rootReducer from './store/rootReducer'
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import { Action } from 'redux'
-import requestExecutor from '../helpers/requestExecutor'
+import { Action, AnyAction } from 'redux'
 
 declare global {
   type AppState = ReturnType<typeof rootReducer>
 
-  type ThunkResult<R> = ThunkAction<R, AppState, ExtraArguments, Action>
+  type ThunkResult<R> = ThunkAction<R, AppState, {}, AnyAction>
 
-  type ThunkDispatch = ThunkDispatch<AppState, ExtraArguments, Action>
+  type ThunkDispatch = ThunkDispatch<AppState, {}, AnyAction>
 }
