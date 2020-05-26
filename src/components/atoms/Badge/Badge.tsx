ebbb62e5cdfd62f5a5e6dbbe45f355c8bd63cb10
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 19:48:55 +0200

    add badge styled components

---
+++ src/components/atoms/Badge/Badge.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+interface StyledProps {
+  color: 'success' | 'danger'
+}
+
+export default styled.div<StyledProps>`
+  padding: 0.35rem 1rem;
+  border-radius: 2px;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  background-color: ${({ color }) => (color === 'success' ? `green` : 'red')};
+`
