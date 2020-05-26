9e3bcac484aa57815c719d492e83b73e64bad171
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 21:31:51 +0200

    update layout with welcome image

--- src/components/pages/public/Home.tsx
+++ src/components/pages/public/Home.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Image } from '../../atoms/Image'
+import welcomeImage from '../../../assets/images/welcome_cats.svg'
+import { Row, Col } from '../../atoms/Grid'
+import styled from 'styled-components'
 
-const HomePage = () => {
-  return <div>Home Page</div>
+interface StyledProps {
+  className?: string
 }
 
-export default HomePage
+const HomePage: React.FC<StyledProps> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Col xs={12} sm={8} md={6}>
+        <img src={welcomeImage} width='100%' height='auto' alt='Welcome' />
+      </Col>
+    </div>
+  )
+}
+
+export default styled(HomePage)`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
