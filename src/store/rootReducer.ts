2938bce69692b392ee89951b27b9912ff21210c8
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 12:48:37 +0200

    add bookings reducer to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 import { userReducer } from './user'
-
+import { bookingsReducer } from './bookings'
 const rootReducer = combineReducers({
   user: userReducer,
+  bookings: bookingsReducer,
 })
 
 export default rootReducer
