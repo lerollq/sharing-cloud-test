a7877142f8ec4204695eebf2dd48ff333e8b0357
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 09:35:05 +0200

    add Navbar organism

---
+++ src/components/organisms/Navbar/navbar.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { NavMenu } from '../../molecules/NavMenu'
+import { NavBrand } from '../../atoms/NavBrand'
+import { BurgerButton } from '../../molecules/BurgerButton'
+
+interface OwnProps {
+  className?: string
+}
+
+const Navbar: React.FC<OwnProps> = ({ className }) => {
+  const [collapse, setCollapse] = useState(false)
+  return (
+    <nav className={className}>
+      <NavBrand />
+      <BurgerButton onClick={() => setCollapse(!collapse)} color='primary' borderless />
+      <NavMenu show={collapse} />
+    </nav>
+  )
+}
+
+export default styled(Navbar)`
+  padding: 1rem 1.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`
