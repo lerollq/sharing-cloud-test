0eddbf6373ec1a1518343d625079df612a5cdaf4
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 10:42:21 +0200

    clear interval if negativ timeleft
    + add comments
    + call handler props if exists

--- src/components/atoms/CountDown/countDown.tsx
+++ src/components/atoms/CountDown/countDown.tsx
@@ -9,18 +9,22 @@ const CountDown: React.FC<OwnProps> = ({ date, handler }) => {
   const [timeleft, setTimeleft] = useState('')
 
   useEffect(() => {
-    const id = setInterval(() => {
+    const intervalId = setInterval(() => {
       const timeleft = new Date(date).getTime() - new Date().getTime()
-      if (timeleft < 0 && handler) {
-        handler()
+      if (timeleft <= 0) {
+        // Negativ timeleft means the countdown is finished.
+        // First clear interval
+        clearInterval(intervalId)
+        // Then trigger the props handler function if exists
+        handler?.()
       }
       const days = Math.floor(timeleft / (1000 * 60 * 60 * 24))
       const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))
       const seconds = Math.floor((timeleft % (1000 * 60)) / 1000)
       setTimeleft(
-        `${days > 0 ? `${days}d ` : ''}${hours > 0 ? `${hours}h ` : ''}${minutes > 0 ? `${minutes}m ` : ''}${
-          seconds > 0 ? `${seconds}s` : ''
+        `${days > 0 ? `${days}d ` : '0d '}${hours > 0 ? `${hours}h ` : '0h '}${minutes > 0 ? `${minutes}m ` : '0m '}${
+          seconds > 0 ? `${seconds}s` : '0s '
         }`
       )
     }, 1000)
@@ -28,9 +32,9 @@ const CountDown: React.FC<OwnProps> = ({ date, handler }) => {
     return () => {
       // This is required to avoid an angry error from react when component unmount
       // We properly delete the running interval
-      clearInterval(id)
+      clearInterval(intervalId)
     }
-  })
+  }, [])
 
   return <React.Fragment>{timeleft}</React.Fragment>
 }
