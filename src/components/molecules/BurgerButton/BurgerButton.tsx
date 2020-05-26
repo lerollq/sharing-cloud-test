ecd96cd62028b3799621ffc319c25075d550642a
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 09:32:31 +0200

    Add BurgerButton molecule

---
+++ src/components/molecules/BurgerButton/BurgerButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button, ButtonProps } from '../../atoms/Button'
+
+interface OwnProps extends ButtonProps {
+  className?: string
+}
+
+const BurgerButton: React.FC<OwnProps> = ({ className, ...buttonProps }) => (
+  <div className={className}>
+    <Button {...buttonProps} fontSize='x-large' borderless>
+      <i className='fas fa-bars'></i>
+    </Button>
+  </div>
+)
+
+export default styled(BurgerButton)`
+  display: none;
+  margin-left: auto;
+  transition: transform 0.4s;
+  &:hover {
+    transform: rotate(180deg);
+  }
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`
