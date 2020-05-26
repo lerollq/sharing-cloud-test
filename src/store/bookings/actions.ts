71ce47f59feca95012eab7e60948fe3812351111
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 19:47:27 +0200

    add bookings store actions

--- src/store/bookings/actions.ts
+++ src/store/bookings/actions.ts
@@ -1,22 +1,54 @@
 import { api } from '../../api'
-import { ActionTypeKeys } from './types'
+import { ActionTypeKeys, ActionTypes, Booking } from './types'
 
-const getBookingsAsyncAction = (): ThunkResult<Promise<void>> => async (dispatch) => {
+const getBookingsAsyncAction = (): ThunkResult<Promise<ActionTypes>> => async (dispatch) => {
   return api.getBookings().then((response) => {
     if (response.success) {
-      console.log(response.data)
-      dispatch({
-        type: ActionTypeKeys.BOOKINGS_SET,
-        payload: {
-          bookings: response.data,
-        },
-      })
+      dispatch(setBookingsAction(response.data))
     } else {
-      dispatch({ type: ActionTypeKeys.BOOKINGS_SET, payload: { bookings: [] } })
+      // TODO Display notification
+    }
+    return dispatch(setStatusAction(true, false))
+  })
+}
+
+const deleteBookingAsyncAction = (bookingId: string): ThunkResult<Promise<ActionTypes | void>> => async (dispatch) => {
+  return api.deleteBookings(bookingId).then((response) => {
+    if (response.success) {
+      return dispatch(deleteBookingAction(bookingId))
     }
   })
 }
 
+const postBookingAsyncAction = (name: string, duration: number): ThunkResult<Promise<void>> => async (
+  dispatch,
+  getState
+) => {
+  return api.postBooking({ name, duration }).then((response) => {
+    if (response.success) {
+      const userId = getState().user.user?.id
+      dispatch(getBookingsAsyncAction())
+    }
+  })
+}
+
+const deleteBookingAction = (bookingId: string): ActionTypes => ({
+  type: ActionTypeKeys.BOOKING_DELETE,
+  payload: { bookingId: bookingId },
+})
+
+const setStatusAction = (loaded: boolean, loading: boolean): ActionTypes => ({
+  type: ActionTypeKeys.BOOKINGS_SET_STATUS,
+  payload: { loaded, loading },
+})
+
+const setBookingsAction = (bookings: Booking[]): ActionTypes => ({
+  type: ActionTypeKeys.BOOKINGS_SET,
+  payload: { bookings },
+})
+
 export const actions = {
   getBookingsAsyncAction,
+  deleteBookingAsyncAction,
+  postBookingAsyncAction,
 }
