e2ef032485ef7a52ebd7eceaa5265a0d5cbfa4f3
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 19:46:55 +0200

    add selector to get user id

--- src/store/user/selectors.ts
+++ src/store/user/selectors.ts
@@ -3,7 +3,8 @@ import { createSelector } from 'reselect'
 const getState = (state: AppState) => state.user
 
 const selectLoggedIn = createSelector(getState, (state) => state.loggedIn)
-
+const selectUserId = createSelector(getState, (state) => state.user?.id ?? '')
 export const selectors = {
   selectLoggedIn,
+  selectUserId,
 }
