60c62d6533780096c99461e76084a90d5a93601d
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 21:31:15 +0200

    wrap navBrand in link which redirect to home page

--- src/components/atoms/NavBrand/navBrand.tsx
+++ src/components/atoms/NavBrand/navBrand.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from '../Link'
 
 interface OwnProps {
   className?: string
 }
 
-const NavBrand: React.FC<OwnProps> = ({ className }) => <div className={className}>Micro Booking</div>
+const NavBrand: React.FC<OwnProps> = ({ className }) => (
+  <div className={className}>
+    <Link to='/'>Micro Booking</Link>
+  </div>
+)
 
 export default styled(NavBrand)`
-  font-size: 1.5rem;
-  font-weight: bold;
   color: white;
+
+  > a {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 `
