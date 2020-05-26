9fdd1da6dfa007f8375a9acc46c6df4e19358a59
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-26 20:24:02 +0200

    remove react functional component and update div tag to main tag

--- src/components/templates/workspace/workspace.tsx
+++ src/components/templates/workspace/workspace.tsx
@@ -1,13 +1,6 @@
-import React from 'react'
 import styled from 'styled-components'
-import { Countdown } from '../../atoms/CountDown'
 
-interface Props {
-  className?: string
-}
-const Workspace: React.FC<Props> = ({ children, className }) => <div className={className}>{children}</div>
-
-export default styled(Workspace)`
+export default styled.main`
   flex: 1;
   padding: 2rem 0.5rem;
 `
