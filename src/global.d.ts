09d52a2f79b79e815fe2ebd614d4a01954542181
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-27 13:32:51 +0200

    add global types to override DefaultTheme styled interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,25 @@
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { AnyAction } from 'redux'
 import rootReducer from './store/rootReducer'
-
+import 'styled-components'
 declare global {
   type AppState = ReturnType<typeof rootReducer>
 
   type ThunkResult<R> = ThunkAction<R, AppState, any, AnyAction>
 
   type ThunkDispatch = ThunkDispatch<AppState, any, AnyAction>
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    breakpoints: {
+      sm: string
+      md: string
+      lg: string
+      xl: string
+    }
+    colors: {
+      primary: string
+    }
+  }
 }
