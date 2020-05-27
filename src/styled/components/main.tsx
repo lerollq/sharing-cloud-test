500befb6488b5dd26d4688831707744bab1417c2
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-27 13:45:58 +0200

    add styled main component

--- src/styled/components/main.tsx
+++ src/styled/components/main.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
-export default styled.main`
+const Main = styled.main`
+  background-color: purple;
   flex: 1;
   padding: 2rem 0.5rem;
 `
+
+export default Main
