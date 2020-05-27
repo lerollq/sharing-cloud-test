5e7ab2ac13985176855becf01ff3835ccb29353a
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2020-05-27 17:14:30 +0200

    clean imports

--- src/components/organisms/ResourceDetails/ResourceDetails.tsx
+++ src/components/organisms/ResourceDetails/ResourceDetails.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { resourceSelectors, resourceActions } from '../../../store/resource'
 import { LoadingCardBody } from '../../molecules/LoadingCardBody'
-import { bookingsSelectors } from '../../../store/bookings'
 import { Resource } from '../../../store/resource/types'
-import { Badge, Card, CardHeader, Grid } from '../../../styled'
+import { Card, CardHeader, Grid } from '../../../styled'
+import { BadgeResourceAvailability } from '../../atoms/BadgeResourceAvailability'
 
 interface DispatchToProps {
   getResourceAsyncAction(): Promise<void>
 }
 
 interface StateToProps {
-  bookingsTime: Array<{ end: string; start: string }>
   resource: Resource
   loading: boolean
   loaded: boolean
@@ -20,46 +19,19 @@ interface StateToProps {
 type ResourceDetailsProps = StateToProps & DispatchToProps
 
 const ResourceDetails: React.FC<ResourceDetailsProps> = React.memo(
-  ({ loaded, loading, getResourceAsyncAction, resource, bookingsTime }) => {
-    const [available, setAvailable] = useState(false)
-
-    /**
-     * This method check every second if current time is between start & end date of any bookings
-     * And will update current available state if different
-     */
-
-    const checkAvailability = useCallback(() => {
-      const currentTime = new Date().getTime()
-      const isAvailable = !bookingsTime.some(
-        ({ end, start }) => currentTime > new Date(start).getTime() && currentTime < new Date(end).getTime()
-      )
-      if (isAvailable !== available) {
-        setAvailable(isAvailable)
-      }
-    }, [available, bookingsTime])
-
+  ({ loaded, loading, getResourceAsyncAction, resource }) => {
     useEffect(() => {
       // If resource not loaded, call getResourceAsyncAction
       if (!loaded) {
         getResourceAsyncAction()
       }
-
-      // Start interval to check resource availability
-      checkAvailability()
-      const intervalId = setInterval(checkAvailability, 1000)
-
-      return () => {
-        // Properly delete interval
-        clearInterval(intervalId)
-      }
-      // Re-render only if loadedn bookingsTime or available state change
-    }, [loaded, bookingsTime, available, getResourceAsyncAction, checkAvailability])
+    }, [loaded, getResourceAsyncAction])
 
     return (
       <Card>
         <CardHeader alignItems='center' justifyContent='space-between'>
           <h2> {resource.name}</h2>
-          <Badge color={available ? 'success' : 'danger'}>{available ? 'Available' : 'Not Available'}</Badge>
+          <BadgeResourceAvailability />
         </CardHeader>
         <LoadingCardBody loading={loading}>
           <Grid.Row>
@@ -81,7 +53,6 @@ const ResourceDetails: React.FC<ResourceDetailsProps> = React.memo(
 const mapStateToProps = (state: AppState) => ({
   resource: resourceSelectors.selectResource(state),
   ...resourceSelectors.selectStatus(state),
-  bookingsTime: bookingsSelectors.selectBookingsTimes(state),
 })
 
 const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
